3a4c78bbf3bcce6719b0862cef4b9323483d59be
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-06-10 15:16:15 +0530

    added and styled post-details page

--- blog/views.py
+++ blog/views.py
@@ -12,5 +12,5 @@ def posts(request):
     return render(request, "blog/all-posts.html")
 
 
-def post_detail(request):
-    return render(request, "blog/")
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
