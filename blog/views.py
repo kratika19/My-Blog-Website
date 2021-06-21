ff603dace2ad50379ed429f56a017232ce268e57
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-06-21 20:54:13 +0530

    linking comment form

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Post
 from django.views import View
 from .forms import CommentForm
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 
 
 # Create your views here.
@@ -20,12 +22,10 @@ class StartingPageView(ListView):
     ordering = ["-date"]
     context_object_name = 'posts'
 
-
     def get_queryset(self):
         queryset = super().get_queryset()
         data = queryset[:3]
         return data
-
 
 
 # def posts(request):
@@ -50,12 +50,30 @@ class PostsView(ListView):
 #     })
 
 
-class DetailPostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class DetailPostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
